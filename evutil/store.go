72f708c332709c50aa65e022d004464f07b4b47b
Sigalas Alexandros <alxarch@gmail.com>
2019-07-30 23:26:11 +0300

    [evutil] Add MuxScanner and MuxStore helpers

--- evutil/store.go
+++ evutil/store.go
@@ -2,11 +2,11 @@ package evutil
 
 import (
 	"context"
-	"errors"
 	"sync"
 	"time"
 
 	db "github.com/alxarch/evdb"
+	errors "golang.org/x/xerrors"
 )
 
 // StorerFunc is a function that implements Storer interface
@@ -136,4 +136,68 @@ func (tee teeStorer) Store(s *db.Snapshot) error {
 		}
 	}
 	return nil
+}
+
+type MuxStore map[string]db.Storer
+
+func (m MuxStore) Add(s db.Storer, events ...string) MuxStore {
+	if m == nil {
+		m = make(map[string]db.Storer)
+	}
+	for _, event := range events {
+		m[event] = s
+	}
+	return m
+
+}
+func (m MuxStore) Storer(event string) db.Storer {
+	return m[event]
+}
+
+type MuxScanner map[string]db.ScanQuerier
+
+func (m MuxScanner) Add(s db.ScanQuerier, events ...string) MuxScanner {
+	if m == nil {
+		m = make(map[string]db.ScanQuerier)
+	}
+	for _, event := range events {
+		m[event] = s
+	}
+	return m
+
+}
+
+func (m MuxScanner) Scan(ctx context.Context, queries ...db.ScanQuery) (db.Results, error) {
+	queries = db.ScanQueries(queries).Compact()
+	wg := new(sync.WaitGroup)
+	errc := make(chan error, len(queries))
+	var out db.Results
+	var mu sync.Mutex
+	for i := range queries {
+		q := &queries[i]
+		s := m[q.Event]
+		if s == nil {
+			continue
+		}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			// Merge all overlapping queries
+			results, err := s.ScanQuery(ctx, q)
+			if err == nil {
+				mu.Lock()
+				out = append(out, results...)
+				mu.Unlock()
+			}
+			errc <- err
+		}()
+	}
+	wg.Wait()
+	close(errc)
+	for err := range errc {
+		if err != nil {
+			return nil, err
+		}
+	}
+	return out, nil
 }
