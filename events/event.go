904ef8e8cffcf53370f46636194279182b1e9e09
Sigalas Alexandros <alxarch@gmail.com>
2019-08-01 21:46:03 +0300

    [events] Add MergeTask to use with partial events

--- events/event.go
+++ events/event.go
@@ -18,3 +18,40 @@ func NewEvent(name string, labels ...string) *Event {
 
 	return &e
 }
+
+// MergeTask creates a task that merges src to dst filling missing labels with values from static
+func MergeTask(dst, src *Event, static map[string]string) func() {
+	type labelIndex struct {
+		Index int
+		Value string
+	}
+	index := make([]labelIndex, len(dst.Labels))
+	values := make([]string, len(dst.Labels))
+	for i, label := range dst.Labels {
+		index[i].Index = -1
+		index[i].Value = static[label]
+		for j := range src.Labels {
+			if src.Labels[j] == label {
+				index[i].Index = j
+				break
+			}
+		}
+	}
+	var scratch CounterSlice
+	return func() {
+		scratch = src.Counters.Flush(scratch.Reset())
+		for i := range scratch {
+			c := &scratch[i]
+			for j := range values {
+				i := &index[j]
+				if 0 <= i.Index && i.Index < len(c.Values) {
+					values[j] = c.Values[i.Index]
+				} else {
+					values[j] = i.Value
+				}
+			}
+			dst.Add(c.Count, values...)
+		}
+
+	}
+}
