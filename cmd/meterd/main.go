b229381cc4ba0a9147c044aa91706da2147c1ae5
Sigalas Alexandros <alxarch@gmail.com>
2019-08-02 14:25:54 +0300

    [meterd] Add readonly flag

--- cmd/meterd/main.go
+++ cmd/meterd/main.go
@@ -18,15 +18,16 @@ import (
 
 var (
 	addr     = flag.String("addr", ":8080", "HTTP listen address")
+	readOnly = flag.Bool("readonly", false, "Don't create store endpoints")
 	debug    = flag.Bool("debug", false, "Debug logs")
 	basePath = flag.String("basepath", "", "Basepath for URLs")
 	dbURL    = flag.String("db", "badger:///var/lib/meterd", "Database configuration URL")
+	logInfo  = log.New(os.Stdout, "[INFO] ", log.Ldate|log.Ltime)
+	logError = log.New(os.Stderr, "[ERROR] ", log.Ldate|log.Ltime)
 )
 
 func main() {
 	flag.Parse()
-	logInfo := log.New(os.Stdout, "[meterd] ", log.LstdFlags)
-	logError := log.New(os.Stderr, "[meterd] ", log.LstdFlags)
 	events := flag.Args()
 	db, err := evdb.Open(*dbURL, events...)
 	if err != nil {
@@ -50,17 +51,22 @@ func main() {
 		}
 		os.Exit(0)
 	}()
+	var w evdb.Store
+	if !*readOnly {
+		w = db
+
+	}
 	srv := http.Server{
 		Addr:     *addr,
 		ErrorLog: logError,
-		Handler:  evhttp.DefaultMux(db, db),
+		Handler:  evhttp.DefaultMux(db, w),
 	}
 	if prefix := *basePath; prefix != "" {
 		prefix = "/" + strings.Trim(prefix, "/")
 		srv.Handler = http.StripPrefix(prefix, srv.Handler)
 	}
 
-	logInfo.Printf("Listening on %s...\n", srv.Addr)
+	logInfo.Printf("Serving %s on %s...\n", *dbURL, srv.Addr)
 	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		logError.Printf("Server failed: %s\n", err)
 	}
